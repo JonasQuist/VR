12eed56e58c14bef5750d80f4b0b0f6b8c5e3d56
Andreas Nielsen <thesilentwindow@live.dk>
2017-05-01 17:10:52 +0200

    Video Front Page
    Added layout concept for frontpage

---
+++ Models/FrontPageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VR.Models
+{
+    public class FrontPageModel
+    {
+        public List<VideoModel> Videos { get; set; }
+
+
+    }
+}
---
+++ Models/VideoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+
+namespace VR.Models
+{
+    public class VideoModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Path { get; set; }
+        public string ImagePath { get; set; }
+        public Image Thumbnail { get; set; }
+
+    }
+}
